e260aa99952fb543a6568d22343b4103801e3dd8
zsigmondattila <zsigmond.attila@student.ms.sapientia.ro>
2023-02-16 11:08:29 +0200

    labor_01

---
+++ labor_01/Array.cpp
@@ -0,0 +1,37 @@
+//
+// Created by tivad on 2023. 02. 16..
+//
+
+#include "Array.h"
+
+int *initializeArray(int numberOfElements) {
+    return new int[numberOfElements];
+}
+
+void readArray(int *&array, int &numberOfElements, char *input) {
+    ifstream f(input);
+    if(!f) {
+        cout<<"A fájl nem megnyitható!"<<endl;
+        exit(1);
+    }
+    f>>numberOfElements;
+    array = initializeArray(numberOfElements);
+    for (int i = 0; i < numberOfElements; ++i) {
+        f>>array[i];
+    }
+    f.close();
+}
+
+void printArray(int *array, int numberOfElements) {
+    for (int i = 0; i < numberOfElements; ++i) {
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void printReverseArray(int *array, int numberOfElements) {
+    for (int i = numberOfElements-1; i >= 0; --i) {
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
---
+++ labor_01/Array.h
@@ -0,0 +1,18 @@
+//
+// Created by tivad on 2023. 02. 16..
+//
+
+#ifndef LABOR_01_ARRAY_H
+#define LABOR_01_ARRAY_H
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int *initializeArray(int numberOfElements);
+void readArray(int *&array, int & numberOfElements, char input[20]);
+void printArray(int *array, int numberOfElements);
+void printReverseArray(int *array, int numberOfElements);
+
+#endif //LABOR_01_ARRAY_H
